ff00cca31ba33abe7835d134c8053e840f1e17eb
Gev <gev@zoom.am>
2021-05-02 22:31:56 +0400

    connected Voyager admin panel, created Content and Logs models

---
+++ database/migrations/2021_05_02_182725_create_contents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->char('guid', 255);
+            $table->text('title');
+            $table->char('url', 255);
+            $table->text('short_description');
+            $table->dateTime('date_published');
+            $table->char('author', 255)->nullable();
+            $table->char('image', 255)->nullable();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
